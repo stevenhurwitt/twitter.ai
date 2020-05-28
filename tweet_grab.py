45b29d3fe85b604ff0626b67430d414f593906e9
Steven Hurwitt <stevenhurwitt@gmail.com>
2020-05-28 17:36:37 -0500

    init_repo

--- tweet_grab.py
+++ tweet_grab.py
@@ -1,6 +1,7 @@
 import os
 import tweepy
 import logging
+import logging.handlers
 import pprint
 import json
 import datetime
@@ -8,11 +9,24 @@ import pandas as pd
 import numpy as np
 
 #generate log file, init tweepy api, init pprinter
-print('working in directory: %s' % os.getcwd())
+base = os.getcwd()
+data_dir = os.path.join(base, 'data')
+print('working in directory: %s' % base)
+
 pp = pprint.PrettyPrinter(indent = 1)
-logging.basicConfig(filename = 'twitter.log', level = logging.DEBUG)
-auth = tweepy.OAuthHandler('vDmSiSjkot8pbnf4Z7eUmNCaW', 'dBcYnqMwWaTDx76weHrybngCsfszZQ4TinxsCjZrgo4hR2nlWH')
-auth.set_access_token('258542490-FD117iVPwblz8uGppXC1M8blIOzZLPEMTw7ATIv8', 'hCbeVJA9Rlif9rYGHlFMUEbJl5nzKlZ5GjVbxQ5QEQdGX')
+
+should_roll_over = os.path.isfile('tweet_grab.log')
+handler = logging.handlers.RotatingFileHandler('tweet_grab.log', mode='w', backupCount=1)
+if should_roll_over: 
+    handler.doRollover()
+logging.basicConfig(filename = 'tweet_grab.log', level = logging.DEBUG)
+
+with open('/media/steven/big_boi/creds_tweepy.json', 'r') as f:
+    tweepy_creds = json.load(f)
+f.close()
+    
+auth = tweepy.OAuthHandler(tweepy_creds['consumer_key'], tweepy_creds['consumer_secret'])
+auth.set_access_token(tweepy_creds['access_key'], tweepy_creds['access_secret'])
 
 api = tweepy.API(auth)
 logging.info('authorized API w/ client id and secret.')
@@ -30,11 +44,17 @@ def get_tweets_per_user(user, pages):
         try:
             tweets = api.user_timeline(screen_name = user, page = p)
             for t in tweets:
-                tweet_dict = dict([('text', t.text), ('user', user), ('date', t.created_at), ('fav_count', t.favorite_count), ('retweet_count', t.retweet_count), ('retweet', t.retweeted)])
-                tweet_info.append((i, tweet_dict))
-                i += 1
+                try:
+                    tweet_dict = dict([('text', t.text), ('user', user), ('date', t.created_at), ('fav_count', t.favorite_count), ('retweet_count', t.retweet_count), ('retweet', t.retweeted)])
+                    tweet_info.append((i, tweet_dict))
+                    i += 1
+                except:
+                    print('error with tweet {}.'.format(i))
         except:
-            print('error with page {}'.format(p))
+            output = 'error with page {}'.format(p)
+            print(output)
+            logging.error(output)
+
     tweet_info = dict(tweet_info)
     tweet_info_json = json.dumps(tweet_info, indent = 1, sort_keys = False, default = str)
     json_fname = '%s_tweet_dump.json' % user
@@ -42,7 +62,6 @@ def get_tweets_per_user(user, pages):
     with open(json_fname, 'w') as f:
         json.dump(tweet_info_json, f)
     
-    print('wrote %s tweets as %s' % (user, json_fname))
     
     tweets_df = pd.read_json(tweet_info_json, orient = 'index')
     tweets_df.sort_index(axis = 0, inplace = True)
@@ -56,21 +75,29 @@ def get_tweets_per_user(user, pages):
     return(tweets_df)
 
 #concatenate into master df, write to file
-master = []
+def main():
+    master = []
 
-for u in users:
-    tweets_df = get_tweets_per_user(u, 100)
-    master.append(tweets_df)
+    for u in users:
+        os.chdir(data_dir)
+        tweets_df = get_tweets_per_user(u, 100)
+        master.append(tweets_df)
+        os.chdir(base)
 
-master_df = pd.concat(master)
-master_fname = 'master_tweet_dump.csv'
-master_df.to_csv(master_fname, index = False)
-logging.info('wrote master .csv file.')
-print('wrote master .csv file.')
+    os.chdir(data_dir)
+    master_df = pd.concat(master)
+    master_fname = 'master_tweet_dump.csv'
+    master_df.to_csv(master_fname, index = False)
+    logging.info('wrote master .csv file.')
+    print('wrote master .csv file.')
 
-#cleanse master data file (exclude retweets)
-not_retweet = [r == False for r in master_df.retweet]
-master_df = master_df[not_retweet]
-master_df.reset_index(drop = True)
-master_df.to_csv('master_data_clean.csv', index = False)
-print('wrote cleaned master file to .csv.')
+    #cleanse master data file (exclude retweets)
+    not_retweet = [r == False for r in master_df.retweet]
+    master_df = master_df[not_retweet]
+    master_df.reset_index(drop = True)
+    master_df.to_csv('master_data_clean.csv', index = False)
+    print('wrote cleaned master file to .csv.')
+    os.chdir(base)
+
+if __name__ == "__main__":
+    main()
--- twitter_bq_upload.py
+++ twitter_bq_upload.py
@@ -1,9 +1,122 @@
 import os
 import logging
+import logging.handlers
 import pprint
 import json
-import flask
 import datetime
 import pandas as pd 
 import numpy as np 
+import google.auth
+from google.oauth2 import service_account
 from google.cloud import bigquery
+from google.cloud import bigquery_storage
+
+pp = pprint.PrettyPrinter(indent = 1)
+
+should_roll_over = os.path.isfile('twitter_bq_upload.log')
+handler = logging.handlers.RotatingFileHandler('twitter_bq_upload.log', mode='w', backupCount=1)
+if should_roll_over: 
+    handler.doRollover()
+logging.basicConfig(filename = 'twitter_bq_upload.log', level = logging.DEBUG)
+base = os.getcwd()
+data_dir = os.path.join(base, 'data')
+os.chdir(data_dir)
+
+#init bq client
+creds_fname = '/media/steven/big_boi/creds_google.json'
+client = bigquery.Client.from_service_account_json(creds_fname)
+bqstorageclient = bigquery_storage.BigQueryStorageClient.from_service_account_json(creds_fname)
+logging.info('initialized bigquery client.')
+print('imported modules successfully.')
+
+### read data currently in bq table
+def read_bq_table():
+
+    #read current bq table
+    QUERY = "SELECT * FROM `symbolic-bit-277217.basement_dude_tweets.tweets_master`;"
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+
+    results = client.query(QUERY).result()
+    bq_output = results.to_dataframe()
+    return(bq_output)
+
+### function to subset data from master (A) not in bq table (B)
+def subset_df(A, B):
+    to_upload = pd.concat([A.drop(columns = 'date'), B.drop(columns = 'date')]).drop_duplicates(keep = False)
+    print('master len: {}, bq table len: {}, subset df len: {}'.format(A.shape[0], B.shape[0], to_upload.shape[0]))
+    print('if master len < bq table len, data size has decreased and there is none to upload.')
+    to_upload = pd.concat([to_upload, A.date], axis = 1, join = 'inner')
+    to_upload = to_upload[['text', 'user', 'date', 'fav_count', 'retweet_count', 'retweet']]
+    to_upload.reset_index(drop = True, inplace = True)
+    to_upload.to_csv('master_data_upload.csv', index = False)
+
+    output = 'adding {} records to bq db.'.format(to_upload.shape[0])
+    print(output)
+    logging.info(output)
+    return(to_upload)
+
+### upload new data to bq table
+def bq_upload():
+    #set table ref
+    table_ref = client.dataset('basement_dude_tweets').table('tweets_master')
+
+    #create job_config
+    job_config = bigquery.LoadJobConfig()
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
+    job_config.skip_leading_rows = 1
+
+    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.auto_detect = True
+    job_config.allow_jagged_rows = True
+    job_config.allow_quoted_newlines = True
+
+    #upload local file to bq
+    filename = '/media/steven/big_boi/twitter/master_data_upload.csv'
+    with open(filename, "rb") as source_file:
+        load_job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+    print("Starting job {}".format(load_job.job_id))
+
+    try:
+        load_job.result()  # Waits for table load to complete.
+        print("Job finished.")
+
+        destination_table = client.get_table(table_ref)
+        output = "Loaded {} rows.".format(destination_table.num_rows)
+        print(output)
+        logging.info(output)
+    
+    except:
+        print('Job failed:')
+        for j in load_job.errors:
+            logging.error(j)
+
+def main():
+    try:
+        #read master data .csv
+        master = pd.read_csv('master_data_clean.csv')
+    except:
+        print('file import error.')
+
+    try:
+        #read bq table
+        bq_output = read_bq_table()
+    except:
+        print('bigquery read error.')
+
+    try:
+        #subset data to add
+        to_upload = subset_df(master, bq_output)
+    except:
+        print('data subset error.')
+        logging.warning('no new data to upload.')
+
+    try:
+        #upload new data to bq table
+        bq_upload()
+    except:
+        print('bigquery upload error.')
+        logging.warning('bq upload failed.')
+
+if __name__ == "__main__":
+    main()
