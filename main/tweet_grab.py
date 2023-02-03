909255dd0f5b01a4baa0328f1f9351e61bf12be0
steven hurwitt <stevenhurwitt@gmail.com>
2023-02-03 17:36:25 -0600

    updates

--- main/tweet_grab.py
+++ main/tweet_grab.py
@@ -4,38 +4,106 @@ import logging
 import logging.handlers
 import pprint
 import json
-import datetime
+import time
+import datetime as dt
 import pandas as pd
 import numpy as np
+import boto3
+import botocore.session
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key, Attr
+print("imported modules.")
 
-#generate log file, init tweepy api, init pprinter
-base = os.getcwd()
-data_dir = os.path.join(base, 'data')
-log_dir = os.path.join(base, 'logs')
-print('working in directory: %s' % base)
+with open("../creds.json", "r") as f:
+    creds = json.load(f)
+    f.close()
+    print("read creds.json")
 
-pp = pprint.PrettyPrinter(indent = 1)
+def get_followers(user):
 
-os.chdir(log_dir)
-should_roll_over = os.path.isfile('tweet_grab.log')
-handler = logging.handlers.RotatingFileHandler('tweet_grab.log', mode='w', backupCount=1)
-if should_roll_over: 
-    handler.doRollover()
-logging.basicConfig(filename = 'tweet_grab.log', level = logging.DEBUG)
-os.chdir(base)
 
-with open('creds_tweepy.json', 'r') as f:
-    tweepy_creds = json.load(f)
-f.close()
+    with open("../creds.json", "r") as f:
+        creds = json.load(f)
+        f.close()
+        print("read creds.json")
+
+    following = []
+    auth = tweepy.OAuthHandler(creds['twitter-api-key'], creds['twitter-secret-key'])
+    auth.set_access_token(creds['twitter-access-token'], creds['twitter-secret-access'])
+
+    api = tweepy.API(auth)
+    friends_response = api.get_followers(screen_name = user, count = 100, cursor = -1)
+    following.extend([t.screen_name for t in friends_response[0]])
+    next_cursor = friends_response[1][1]
+    print("got most recent 100 following")
+
+    while next_cursor != 0:
+        try:
+            friends_response = api.get_followers(screen_name = user, count = 100, cursor = next_cursor)
+            following.extend([t.screen_name for t in friends_response[0]])
+            next_cursor = friends_response[1][1]
+            print("got next 100 following")
+        
+        except:
+            time.sleep(60*15)
+
+    return(following)
+
+def batch_put_response(response, table):
+    with table.batch_writer(overwrite_by_pkeys=["user", "created_at"]) as batch:
+        for r in response:
+            try:
+                batch.put_item(Item = {
+                    "user": r.user.screen_name,
+                    "created_at": r.created_at.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                    "tweet": r.text,
+                    "id": r.id,
+                    "retweets": r.retweet_count,
+                    "favorites": r.favorite_count,
+                    "inserted_at": dt.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+                })
+        
+            except ClientError as err:
+                print("Couldn't load data into table {}. Here's why: {}: {}".format(table.name, err.response['Error']['Code'], err.response['Error']['Message']))
+                raise
+
+def get_tweets(user, table):
+    print("scraping tweets for user: {}".format(user))
+    auth = tweepy.OAuthHandler(creds['twitter-api-key'], creds['twitter-secret-key'])
+    auth.set_access_token(creds['twitter-access-token'], creds['twitter-secret-access'])
+
+    api = tweepy.API(auth)
+    timeline_response = api.user_timeline(screen_name = user, count = 25)
+    n = len(timeline_response)
+    last_id = timeline_response[n-1]._json["id"]
+    i = 0
+
+    print("putting first batch")
+    batch_put_response(timeline_response, table)
+    print("inserted first batch to dynamodb successfully.")
+    i += 1
+
+    while i < 25:
+        try:
+            timeline_response = api.user_timeline(screen_name = user, count = 25, max_id = last_id)
+            n = len(timeline_response)
+            last_id = timeline_response[n-1]._json["id"]
+
+            batch_put_response(timeline_response, table)
+            i += 1
+
+            if (i > 0 and (i % 5) == 0):
+                print("inserted batch {} to dynamodb successfully.".format(i+1))
     
-auth = tweepy.OAuthHandler(tweepy_creds['consumer_key'], tweepy_creds['consumer_secret'])
-auth.set_access_token(tweepy_creds['access_key'], tweepy_creds['access_secret'])
+        except Exception as e:
+            print(e)
+            if "429" in str(e):
+                print("sleeping for 5 minutes")
+                time.sleep(60*5)
 
-api = tweepy.API(auth)
-logging.info('authorized API w/ client id and secret.')
-
-#set users to scrape tweets from
-users = ['xanax_princess_', 'slpyboy', 'AristocratAlex', 'timpc9213', 'cd3lisi', 'ChriMoulto', 'uhnonimouse', 'Bean_glitch']
+            else:
+                print("sleeping for a minute")
+                time.sleep(60)
 
 #scrape pages of tweets per users
 #save metadata: tweet, user, retweets, favorites, is_retweet, date
@@ -45,6 +113,10 @@ def get_tweets_per_user(user, pages):
     tweet_info = []
     for p in range(0,pages):
         try:
+            auth = tweepy.OAuthHandler(creds['twitter-api-key'], creds['twitter-secret-key'])
+            auth.set_access_token(creds['twitter-access-token'], creds['twitter-secret-access'])
+
+            api = tweepy.API(auth)
             tweets = api.user_timeline(screen_name = user, page = p)
             for t in tweets:
                 try:
@@ -79,30 +151,51 @@ def get_tweets_per_user(user, pages):
 
 #concatenate into master df, write to file
 def main():
-    master = []
+    try:
+        try:
+            client = boto3.client('dynamodb',
+                endpoint_url = "http://{}:{}".format(creds["host"], creds["port"]),
+                aws_access_key_id=creds["aws-access-key"],
+                aws_secret_access_key=creds["aws-secret-access-key"],
+                region_name='us-east-2')
 
-    for u in users:
-        os.chdir(data_dir)
-        tweets_df = get_tweets_per_user(u, 100)
-        master.append(tweets_df)
-        os.chdir(base)
+            dynamodb = boto3.resource('dynamodb', \
+                        endpoint_url = "http://{}:{}".format(creds["host"], creds["port"]), \
+                        region_name='us-east-2')
 
-    os.chdir(data_dir)
-    master_df = pd.concat(master)
-    master_fname = 'master_tweet_dump.csv'
-    master_df.to_csv(master_fname, index = False)
-    logging.info('wrote master .csv file.')
-    print('wrote master .csv file.')
+            session = botocore.session.get_session()
+            dynamodb_session = session.create_client('dynamodb', \
+                                region_name='us-east-2',
+                                endpoint_url = "http://{}:{}".format(creds["host"], creds["port"]),
+                                aws_access_key_id=creds["aws-access-key"],
+                                aws_secret_access_key=creds["aws-secret-access-key"]) # low-level client
 
-    #cleanse master data file (exclude retweets)
-    not_retweet = [r == False for r in master_df.retweet]
-    master_df = master_df[not_retweet]
-    master_df.reset_index(drop = True)
-    master_df.to_csv('master_data_clean.csv', index = False)
-    print('wrote cleaned master file to .csv.')
-    os.chdir(base)
+            tweets = dynamodb.Table("tweets")
 
-    # write to adls data lake
+            print("created dynamo client.")
+
+        except Exception as e:
+            print(e)
+
+        my_following = get_followers("xanax_princess_")
+        print(len(my_following))
+
+    except Exception as e:
+        print("Exception: {}".format(e))
+
+    results = []
+    for f in my_following:
+        try:
+            output = get_tweets(f, tweets)
+            results.append(output)
+            time.sleep(5)
+
+        except KeyboardInterrupt as k:
+            print(k)
+
+    df = pd.DataFrame(results)
+    df.head()
+    df.shape
 
 if __name__ == "__main__":
     main()
