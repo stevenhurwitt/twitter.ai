1eb6b8f8e7493702a589d0a9d75c8f2e0897f12a
steven hurwitt <stevenhurwitt@gmail.com>
2023-05-17 13:51:53 -0500

    update glue script

--- src/main/dynamo-twitter-glue.py
+++ src/main/dynamo-twitter-glue.py
@@ -18,9 +18,11 @@ datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "twitter"
 
 applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("favorites", "bigint", "favorites", "bigint"), ("created_at", "timestamp", "created_at", "timestamp"), ("id", "bigint", "id", "bigint"), ("tweets", "string", "tweets", "string"), ("retweets", "bigint", "retweets", "bigint"), ("inserted_at", "timestamp", "inserted_at", "timestamp"), ("user", "string", "user", "string")], transformation_ctx = "applymapping1")
 
-resolvechoice3 = ResolveChoice.apply(frame = applymapping1, choice = "MATCH_CATALOG", database = "twitter", table_name = "tweets", transformation_ctx = "resolvechoice3")
+selectfields2 = SelectFields.apply(frame = applymapping1, paths = ["favorites", "created_at", "id", "tweets", "retweets", "inserted_at", "user"], transformation_ctx = "selectfields2")
+
+resolvechoice3 = ResolveChoice.apply(frame = selectfields2, choice = "MATCH_CATALOG", database = "twitter", table_name = "ddb_tweets", transformation_ctx = "resolvechoice3")
 
 resolvechoice4 = ResolveChoice.apply(frame = resolvechoice3, choice = "make_struct", transformation_ctx = "resolvechoice4")
 
-datasink5 = glueContext.write_dynamic_frame.from_catalog(frame = resolvechoice4, database = "twitter", table_name = "tweets", transformation_ctx = "datasink5")
+datasink5 = glueContext.write_dynamic_frame.from_catalog(frame = resolvechoice4, database = "twitter", table_name = "ddb_tweets", transformation_ctx = "datasink5")
 job.commit()
